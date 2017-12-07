4b6a4e88ab6d8990cf6c5162fc9ce620e41fcdb0
Eason Peng <pengyc@rc.inesa.com>
2017-12-07 17:38:27 +0800

    vue learn next server steps

--- scripts/vue_component.js
+++ scripts/vue_component.js
@@ -1,10 +1,31 @@
 /**
  * vm -> viewModel
  */
-var data = { a: 1 }
-
-var vm = new Vue({
-    el:"#app",
-    data:data
-})
+// var vm = new Vue({
+//     el: "#app",
+//     data: { a: 1 }
+// });
 // #watch 是一个实例方法
+
+var vm1 = new Vue({
+    el: '#example',
+    data: {
+        message: Date.now()
+    },
+    computed: {
+        // 计算属性的 getter
+        reversedMessage: function () {
+            // `this` 指向 vm 实例
+            // return this.message.split('').reverse().join('')
+            return Date.now();
+        }
+    },
+    methods: {
+        reversedMessage1: function () {
+            // return this.message.split('').reverse().join('')
+            return Date.now();
+        }
+    }
+})
+
+
