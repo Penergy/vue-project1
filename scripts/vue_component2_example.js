3687ddabb99a2d32fe2d2ca1277e897c7a4cc79c
Eason Peng <pengyc@rc.inesa.com>
2017-12-12 17:34:45 +0800

    传递对象

--- scripts/vue_component2_example.js
+++ scripts/vue_component2_example.js
@@ -1,21 +1,53 @@
-// register a component
-var child = {
-    template: '<div> A local custom component!</div>',
-    data: {
-
+// var data = { counter: 0 }
+Vue.component('simple-counter', {
+    template: '<button v-on:click="counter += 1">{{ counter }}</button>',
+    // 技术上 data 的确是一个函数了，因此 Vue 不会警告，
+    // 但是我们却给每个组件实例返回了同一个对象的引用
+    data: function () {
+        return { counter: 0 }
     }
-}
-Vue.component('my-component', {
-    template: '<div> A custom component!</div>'
 })
 
-var example_vm = new Vue({
-    el: "#example",
-
+new Vue({
+    el: '#example2'
 })
-var example2_vm = new Vue({
-    el: "#example2",
-    components: {
-        'my-component-2': child
+
+Vue.component('child', {
+    // 声明 props
+    props: ['myMessage'],
+    // 就像 data 一样，prop 也可以在模板中使用
+    // 同样也可以在 vm 实例中通过 this.message 来使用
+    template: '<span>{{ myMessage }}</span>'
+})
+var prop_vm = new Vue({
+    el: "#prop-div"
+})
+
+Vue.component('child2', {
+    // 声明 props
+    props: ['myMessage2'],
+    // 就像 data 一样，prop 也可以在模板中使用
+    // 同样也可以在 vm 实例中通过 this.message 来使用
+    template: '<span>{{ myMessage2 }}</span>'
+})
+var prop2_vm = new Vue({
+    el: "#dynamic-prop-div",
+    data: {
+        parentMsg: "testworld!"
+    }
+})
+
+Vue.component('todo-item',{
+    prop:['text','isComplete'],
+    template: '<span>{{ isComplete }}</span>'
+})
+
+var prop3_vm = new Vue({
+    el:"#object-prop-div",
+    data: {
+        todo:{
+            text: 'Learn Vue',
+            isComplete: false
+        }
     }
 })
