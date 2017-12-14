d6e9bdaef9e552bb10d85586407ab6aacf7851c8
Eason Peng <pengyc@rc.inesa.com>
2017-12-14 17:21:15 +0800

    多个组件过渡

---
+++ scripts/vue_multi_move_example.js
@@ -0,0 +1,42 @@
+var vm = new Vue({
+    el: "#example-1",
+    data: {
+        items: [
+
+        ]
+    }
+})
+var no_mode_vm = new Vue({
+    el: "#no-mode-demo",
+    data: {
+        isEditing: true
+    }
+})
+
+var no_mode_absolute_vm = new Vue({
+    el: '#no-mode-absolute-demo',
+    data: {
+        isEditing: true
+    }
+})
+
+var no_mode_translate_vm = new Vue({
+    el: '#no-mode-translate-demo',
+    data: {
+        isEditing: true
+    }
+})
+
+var with_mode_vm = new Vue({
+    el: '#with-mode-demo',
+    data: {
+        isEditing: true
+    }
+})
+
+var in_out_mode_vm = new Vue({
+    el: '#in-out-translate-demo',
+    data:{
+        isEditing: true
+    }
+})
