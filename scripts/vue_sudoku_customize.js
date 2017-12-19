e9c38cebfafbcc474aa4c6a1614cfa9b32860bdf
yicheng <yicheng_peng@hotmail.com>
2017-12-19 22:42:31 +0800

    database error return

--- scripts/vue_sudoku_customize.js
+++ scripts/vue_sudoku_customize.js
@@ -16,7 +16,7 @@ var vm = new Vue({
     },
     created: function () {
         var vm = this
-        axios.get('http://127.0.0.1:8080/images/2')
+        axios.get('http://localhost:8080/images/2')
             .then(function (response) {
                 // vm.answer = _.capitalize(response.data.answer)
                 var hexValues = eval('(' + response.data + ')');
@@ -36,9 +36,16 @@ var vm = new Vue({
                 this.test.push(this.cells[cell].colorHex)
             }
             var test1 = JSON.stringify(this.test)
-            console.log(test1);
-            writeFile('test.txt',test1)
-
+            axios.post('http://localhost:8080/images', {
+                name: 'Fred',
+                hex: test1
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
         }
     }
 })
@@ -47,12 +54,3 @@ function randomColor() {
     var colorStr = Math.floor(Math.random() * 0xFFFFFF).toString(16).toUpperCase();
     return "#" + "000000".substring(0, 6 - colorStr) + colorStr;
 }
-
-function writeFile(filename, filecontent) {
-    var fso, f, s;
-    fso = new ActiveXObject("Scripting.FileSystemObject");
-    f = fso.OpenTextFile(filename, 8, true);
-    f.WriteLine(filecontent);
-    f.Close();
-    alert('write ok');
-} 
